9190f290072e9884481b8ec1e26d57794a00edbb
gentabahananagari <gentapossenti@gmail.com>
2026-02-23 09:43:41 +0700

    feat: change page title and added icon tab

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,15 @@
 import "./globals.css";
 import { Navbar } from "@/components/layout/navbar";
 import { Footer } from "@/components/layout/footer";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: {
+    default: "Genta Bahana Nagari",
+    template: "About | Genta Bahana Nagari",
+  },
+  description: "Genta's personal portfolio",
+}
 
 export default function RootLayout({
   children,
