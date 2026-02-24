05332cae98dc12b5ca7b9d388113aad19c25eb54
gentabahananagari <gentapossenti@gmail.com>
2026-02-24 09:10:29 +0700

    feat: change sections to page path

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,15 +1,9 @@
 import { Hero } from "@/components/sections/hero";
-import { About } from "@/components/sections/about";
-import { Resume } from "@/components/sections/resume";
-import { Contact } from "@/components/sections/contact";
 
 export default function Home() {
   return (
     <>
       <Hero />
-      <About />
-      <Resume />
-      <Contact />
     </>
   );
 }
