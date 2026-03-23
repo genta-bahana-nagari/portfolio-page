b3a5086de71527721c305b87f19b3df27dd70e3d
gentabahananagari <gentapossenti@gmail.com>
2026-03-23 23:19:00 +0700

    remove: unused data content

--- src/content/certification/index.ts
+++
@@ -1,9 +0,0 @@
-export const certifications = [
-  {
-    name: "MikroTik Certified Network Associate",
-    institution: "MikroTik",
-    issued_at: "May 2025",
-    preview: "/images/certifications/MTCNA.jpg",
-    link: "https://mikrotik.com/training/certificates/c457176cc550acd385fb",
-  },
-];
