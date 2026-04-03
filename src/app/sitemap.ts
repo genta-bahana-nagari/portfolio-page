1f7e64671d026665b2b209c6deb8ba58ab605035
gentabahananagari <gentapossenti@gmail.com>
2026-04-03 20:09:12 +0700

    add: guestbook sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -22,5 +22,11 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "monthly",
       priority: 0.7
     },
+    {
+      url: `${baseUrl}/guestbook`,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.9
+    },
   ];
 }
