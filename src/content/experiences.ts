31ada1771b287260c06c81a05c70b2420e2d8b72
genta-bahana-nagari <gentapossenti@gmail.com>
2026-05-03 17:30:38 +0700

    change experiences, remove irrelevant

--- src/content/experiences.ts
+++ src/content/experiences.ts
@@ -5,7 +5,7 @@ export const experiences: Experience[] = [
     title: "Full Stack Engineer",
     type: "Internship",
     company: "PT Gamatechno Indonesia",
-    period: "Jul 2025 – present",
+    period: "Jul 2025 – Apr 2026",
     description:
       "Developing and maintaining full-stack web applications in a collaborative engineering team.",
     logo: "/icons/Gamatechno.jpg",
@@ -18,23 +18,5 @@ export const experiences: Experience[] = [
     description:
       "Built real-world full-stack projects and contributed as backend developer in a capstone team project.",
     logo: "/icons/coding-camp.jpeg",
-  },
-  {
-    title: "Public Relations & Performer",
-    type: "School Organization",
-    company: "PAKS Stembayo",
-    period: "Jul 2023 – Jun 2024",
-    description:
-    "Performed in traditional gamelan orchestra and managed event communication and coordination.",
-    logo: "/icons/PAKS-Stembayo.png",
-  },
-  {
-    title: "Public Relations Officer",
-    type: "School Organization",
-    company: "Karawitan Stembayo",
-    period: "Jul 2023 – Jun 2024",
-    description:
-      "Supported cultural performances and managed outreach for school and community events.",
-    logo: "/icons/Karawitan-Stembayo.png",
   },
 ];
