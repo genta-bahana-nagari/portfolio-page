c1c7c81d004653b5ea1b300ff8d914dc8ccea5db
genta-bahana-nagari <gentapossenti@gmail.com>
2026-05-13 10:42:43 +0700

    fix: missing / in robots.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,7 +7,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: [
       {
         userAgent: "*",
-        allow: ["/", "/project", "experience"],
+        allow: ["/", "/project", "/experience"],
         disallow: ["/guestbook"],
       },
     ],
